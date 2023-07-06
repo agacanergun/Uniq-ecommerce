b221b106dbba6d60aa5fcd0d0b2be700c6b76235
Ağacan <agcannn@gmail.com>
2023-07-06 11:00:55 +0300

    update

--- Uniq/Uniq.WebUI/Program.cs
+++ Uniq/Uniq.WebUI/Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Uniq.BL.Repositories;
+using Uniq.DAL.Contexts;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(SqlRepository<>));
+builder.Services.AddDbContext<SqlContext>(options =>
+          options.UseSqlServer(builder.Configuration.GetConnectionString("CS1")));
+//custom authentication in .net core
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(opt =>
+{
+    opt.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    opt.LoginPath = "/admin/Login";
+    opt.LogoutPath = "/admin/Logout";
+});
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseStatusCodePagesWithRedirects("/hata/{0}");
+}
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllerRoute(name: "admin", pattern: "{area:exists}/{controller=home}/{action=index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=home}/{action=index}/{id?}");
 
 app.Run();
