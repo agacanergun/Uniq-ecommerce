39b7b0812e9c44ef169aae9c6cc4e7a562fa9301
Ağacan <agcannn@gmail.com>
2023-07-06 10:47:12 +0300

    update

---
+++ Uniq/Uniq.DAL/Contexts/SqlContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniq.DAL.Contexts
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
