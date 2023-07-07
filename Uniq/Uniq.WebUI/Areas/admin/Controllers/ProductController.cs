87e6c02c1f86c2413724da2f8951d1770e5f1242
Ağacan <agcannn@gmail.com>
2023-07-07 16:48:46 +0300

    update

--- Uniq/Uniq.WebUI/Areas/admin/Controllers/ProductController.cs
+++ Uniq/Uniq.WebUI/Areas/admin/Controllers/ProductController.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Uniq.BL.Repositories;
+using Uniq.DAL.Entities;
 
 namespace Uniq.WebUI.Areas.admin.Controllers
 {
     [Area("admin"), Authorize(AuthenticationSchemes = "UniqAdminAuth")]
     public class ProductController : Controller
     {
+        IRepository<Product> repoProduct;
+        public ProductController(IRepository<Product> repoProduct)
+        {
+            this.repoProduct = repoProduct;
+        }
+        [Route("admin/urunler")]
         public IActionResult Index()
         {
-            return View();
+            
+            var response = repoProduct.GetAll().OrderBy(x => x.DisplayIndex).Include(x => x.ProductPictures).Include(x=>x.ProductCategories).ToList();
+            return View(response);
         }
     }
 }
