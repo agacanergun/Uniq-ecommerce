f3a92a7d58367098ca88cec75e780565a65ae65f
Ağacan <agcannn@gmail.com>
2023-07-07 14:04:53 +0300

    update

---
+++ Uniq/Uniq.WebUI/Areas/admin/Controllers/CustomerServiceInstitutionalController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Uniq.BL.Repositories;
+using Uniq.DAL.Entities;
+
+namespace Uniq.WebUI.Areas.admin.Controllers
+{
+    [Area("admin"), Authorize(AuthenticationSchemes = "UniqAdminAuth")]
+    public class CustomerServiceInstitutionalController : Controller
+    {
+        IRepository<CustomerServiceInstitutional> repoCustomerServiceInstitutional;
+        public CustomerServiceInstitutionalController(IRepository<CustomerServiceInstitutional> repoCustomerServiceInstitutional)
+        {
+            this.repoCustomerServiceInstitutional = repoCustomerServiceInstitutional;
+        }
+        [Route("admin/müsterihizmetlerikurumsal")]
+        public IActionResult Index()
+        {
+            var response = repoCustomerServiceInstitutional.GetAll().OrderBy(x => x.DisplayIndex).ToList();
+            return View(response);
+        }
+        [Route("admin/müsterihizmetlerikurumsal/ekle")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [Route("admin/müsterihizmetlerikurumsal/ekle"), HttpPost]
+        public async Task<IActionResult> Add(CustomerServiceInstitutional CustomerServiceInstitutional)
+        {
+            if (ModelState.IsValid)
+            {
+                await repoCustomerServiceInstitutional.Add(CustomerServiceInstitutional);
+                return Redirect("/admin/müsterihizmetlerikurumsal");
+            }
+            ViewBag.Error = "Ekleme İşlemi Başarısız";
+            return View(CustomerServiceInstitutional);
+        }
+        [Route("admin/müsterihizmetlerikurumsal/guncelle")]
+        public IActionResult Update(int id)
+        {
+            return View(repoCustomerServiceInstitutional.GetBy(x => x.ID == id));
+        }
+        [Route("admin/müsterihizmetlerikurumsal/guncelle"), HttpPost]
+        public async Task<IActionResult> Update(CustomerServiceInstitutional CustomerServiceInstitutional)
+        {
+            if (ModelState.IsValid)
+            {
+                await repoCustomerServiceInstitutional.Update(CustomerServiceInstitutional);
+                return Redirect("/admin/müsterihizmetlerikurumsal");
+            }
+            ViewBag.Error = "Güncelleme İşlemi Başarısız";
+            return View(CustomerServiceInstitutional);
+        }
+
+        [Route("admin/müsterihizmetlerikurumsal/sil")]
+        public async Task<string> Delete(int id)
+        {
+            CustomerServiceInstitutional CustomerServiceInstitutional = new CustomerServiceInstitutional
+            {
+                ID = id,
+            };
+            await repoCustomerServiceInstitutional.Delete(CustomerServiceInstitutional);
+            return "Ok";
+        }
+    }
+}
