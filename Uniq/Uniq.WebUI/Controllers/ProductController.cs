13f790c63e830c2815b7cd18adc0b0e0e1c5ea13
Ağacan <agcannn@gmail.com>
2023-07-11 14:17:50 +0300

    update

---
+++ Uniq/Uniq.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Uniq.BL.Repositories;
+using Uniq.DAL.Entities;
+
+namespace Uniq.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        IRepository<Product> repoProduct;
+        public ProductController(IRepository<Product> _repoProduct)
+        {
+            repoProduct = _repoProduct;
+        }
+
+        [Route("/urundetay/{name}-{id}")]
+        public IActionResult Index(string name, int id)
+        {
+            var product = repoProduct.GetAll().Include(x => x.ProductPictures).FirstOrDefault(x => x.ID == id);
+            return View();
+        }
+    }
+}
