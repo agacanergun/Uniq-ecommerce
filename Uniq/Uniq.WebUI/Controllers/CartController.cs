2c0e06737d38771b90565ce2e0e0d18ff6f29716
Ağacan <agcannn@gmail.com>
2023-07-12 11:27:30 +0300

    update

---
+++ Uniq/Uniq.WebUI/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uniq.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        [Route("/sepetim")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Uniq/Uniq.WebUI/wwwroot/assets/js/shoppingcart.js
@@ -0,0 +1,39 @@
+﻿(function ($) {
+
+    getCartCounter();
+})(jQuery);
+
+function addCart(productid) {
+    var istenenmiktar = parseInt($(".inputQuantity").val())
+    $.ajax({
+        url: "/sepetim/ekle",
+        type: "POST",
+        data: { productid: productid, quantity: istenenmiktar },
+        success: function (data) {
+            if (data != "") {
+                getCartCounter();
+            }
+        }
+    });
+}
+
+//function getCartCounter() {
+//    $.ajax({
+//        url: "/sepetim/sayiver",
+//        type: "GET",
+//        success: function (data) {
+//            $(".cartCounter").text(data);
+//        }
+//    });
+//}
+
+//function removeCart(productid) {
+//    $.ajax({
+//        url: "/sepetim/sil",
+//        type: "POST",
+//        data: { productid: productid },
+//        success: function (data) {
+//            if (data == "OK") location.href = "/sepetim";
+//        }
+//    });
+//} 
