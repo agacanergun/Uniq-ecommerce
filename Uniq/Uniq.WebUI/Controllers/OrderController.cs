7d9cd0b37057ed9f804849f18a417d6536e46999
Ağacan <agcannn@gmail.com>
2023-07-14 13:14:03 +0300

    update

---
+++ Uniq/Uniq.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using Uniq.BL.Repositories;
+using Uniq.DAL.Entities;
+using Uniq.WebUI.Models;
+using Uniq.WebUI.ViewModels;
+
+namespace Uniq.WebUI.Controllers
+{
+    [Authorize(AuthenticationSchemes = "UniqMemberAuth")]
+    public class OrderController : Controller
+    {
+        IRepository<Shipping> repoShipping;
+        IRepository<CustomerAdresses> repoCustomerAdresses;
+        IRepository<Customer> repoCustomer;
+        public OrderController(IRepository<Shipping> repoShipping, IRepository<CustomerAdresses> repoCustomerAdresses, IRepository<Customer> repoCustomer)
+        {
+            this.repoShipping = repoShipping;
+            this.repoCustomerAdresses = repoCustomerAdresses;
+            this.repoCustomer = repoCustomer;
+        }
+        public IActionResult Index()
+        {
+            var userId = int.Parse(HttpContext.User.FindFirst(ClaimTypes.PrimarySid)?.Value);
+            var customer = repoCustomer.GetBy(x => x.Id == userId);
+            if (customer != null)
+            {
+                if (customer.GuidId.ToString() == HttpContext.User.FindFirst(c => c.Type == "UserGuid")?.Value)
+                {
+                    var carts = JsonConvert.DeserializeObject<List<Cart>>(Request.Cookies["MyCart"]);
+                    decimal totalAmount = 0;
+                    foreach (var item in carts)
+                    {
+                        totalAmount += item.Quantity * item.Price;
+                    }
+                    OrderVM orderVM = new OrderVM
+                    {
+                        TotalAmount = totalAmount,
+                        CustomerAdresses = repoCustomerAdresses.GetAll(x => x.CustomerID == userId).ToList(),
+                        Shippings = repoShipping.GetAll().OrderBy(x => x.DisplayIndex).ToList(),
+                    };
+                    return View(orderVM);
+                }
+                else
+                    return Redirect("/");
+            }
+            else
+                return Redirect("/");
+        }
+    }
+}
---
+++ Uniq/Uniq.WebUI/ViewModels/OrderVM.cs
@@ -0,0 +1,11 @@
+﻿using Uniq.DAL.Entities;
+
+namespace Uniq.WebUI.ViewModels
+{
+    public class OrderVM
+    {
+        public decimal TotalAmount { get; set; }
+        public List<Shipping> Shippings { get; set; }
+        public List<CustomerAdresses> CustomerAdresses { get; set; }
+    }
+}
