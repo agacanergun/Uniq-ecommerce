2e8648d768d6caa2708a81ff9bf7980a23d1ba33
Ağacan <agcannn@gmail.com>
2023-07-17 15:25:15 +0300

    update

---
+++ Uniq/Uniq.WebUI/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Uniq.BL.Repositories;
+using Uniq.DAL.Entities;
+using Uniq.WebUI.ViewModels;
+
+namespace Uniq.WebUI.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        IRepository<Category> repoCategory;
+        public HeaderViewComponent(IRepository<Category> repoCategory)
+        {
+            this.repoCategory = repoCategory;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categories = repoCategory.GetAll().ToList();
+            return View(categories);
+        }
+    }
+}
